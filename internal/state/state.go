43532585e4add9256be05bce1fd6df8c1dd5e93c
Matthew Smart <matthewsmart13@gmail.com>
2024-12-25 17:43:24 -0600

    State logic for initalizing database

---
+++ internal/state/state.go
@@ -0,0 +1,37 @@
+package state
+
+import (
+	"database/sql"
+	"errors"
+	"os"
+
+	"github.com/itsMe-ThatOneGuy/parts-bin/internal/database"
+)
+
+func (s *State) InitDB() error {
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		dbURL = s.Config.DBUrl
+		if dbURL == "" || dbURL == config.DefaultDBurl {
+			return errors.New("db_url not set in either .env or ~/.partsbinconfig.json")
+		}
+		println(".config")
+	} else {
+		println(".env")
+	}
+
+	dbCon, err := sql.Open("postgres", s.Config.DBUrl)
+	if err != nil {
+		return err
+	}
+
+	s.DB = dbCon
+	s.DBQueries = database.New(dbCon)
+	return nil
+}
+
+func (s *State) CloseDB() {
+	if s.DB != nil {
+		s.DB.Close()
+	}
+}
