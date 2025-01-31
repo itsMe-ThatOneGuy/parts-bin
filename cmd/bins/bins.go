7dafa49a0db9894a979ea0f1db5469b25f8b862d
Matthew Smart <matthewsmart13@gmail.com>
2025-01-31 12:27:13 -0600

    bin handler for updating name

--- cmd/bins/bins.go
+++ cmd/bins/bins.go
@@ -75,3 +75,50 @@ func DeleteBin(s *state.State, args []string) (database.Bin, error) {
 
 	return database.Bin{}, errors.New("Required argument flag not provided")
 }
+
+func UpdateBin(s state.State, args []string) (database.Bin, error) {
+	argType := args[0]
+	argBin := args[1]
+	argInputs := args[2:]
+
+	if argType == "-i" {
+		argBin, err := uuid.Parse(argBin)
+		if err != nil {
+			return database.Bin{}, errors.New("Issue parsing UUID")
+		}
+
+		if argInputs[0] == "-bn" {
+			bin, err := s.DBQueries.UpdateBinNameByID(context.Background(),
+				database.UpdateBinNameByIDParams{
+					ID:   argBin,
+					Name: argInputs[1],
+				},
+			)
+			if err != nil {
+				return database.Bin{}, errors.New("Issue renaming bin using id")
+			}
+
+			return bin, nil
+		}
+
+	}
+
+	if argType == "-n" {
+		if argInputs[0] == "-bn" {
+			bin, err := s.DBQueries.UpdateBinNameByName(context.Background(),
+				database.UpdateBinNameByNameParams{
+					Name:   argBin,
+					Name_2: argInputs[1],
+				},
+			)
+			if err != nil {
+				return database.Bin{}, errors.New("Issue renaming bin using name")
+			}
+
+			return bin, nil
+		}
+
+	}
+
+	return database.Bin{}, nil
+}
