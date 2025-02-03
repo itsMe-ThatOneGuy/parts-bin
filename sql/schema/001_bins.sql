c7c4f92cbf4a75265e4699ef333e7a95956f0db2
Matthew Smart <matthewsmart13@gmail.com>
2025-02-03 11:07:29 -0600

    Updated schema

--- sql/schema/001_bins.sql
+++ sql/schema/001_bins.sql
@@ -3,8 +3,11 @@ CREATE TABLE bins (
     id UUID PRIMARY KEY, 
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL UNIQUE,
-    parent_bin UUID DEFAULT NULL
+    name TEXT NOT NULL,
+    parent_bin UUID DEFAULT NULL,
+    FOREIGN KEY (parent_bin) 
+    REFERENCES bins(id) ON DELETE SET NULL,
+    UNIQUE (name, parent_bin)
 );
 
 -- +goose Down
