b5ec5e73e05c73d2fbcbdd461bd59f3f9afa64b6
Matthew Smart <matthewsmart13@gmail.com>
2025-02-03 22:38:19 -0600

    updated bin schema

--- sql/schema/001_bins.sql
+++ sql/schema/001_bins.sql
@@ -4,11 +4,13 @@ CREATE TABLE bins (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL,
-    parent_bin UUID DEFAULT NULL,
-    FOREIGN KEY (parent_bin) 
+    parent_bin UUID DEFAULT NULL 
     REFERENCES bins(id) ON DELETE SET NULL,
-    UNIQUE (name, parent_bin)
+    parent_bin_or_null UUID GENERATED ALWAYS AS (COALESCE(parent_bin, '00000000-0000-0000-0000-000000000000')) STORED
 );
+
+ALTER TABLE bins
+ADD CONSTRAINT bins_name_parent_bin_key UNIQUE (name, parent_bin_or_null);
 
 -- +goose Down
 DROP TABLE bins;
