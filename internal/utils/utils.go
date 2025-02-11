361ae18615ea64f1b9725add3f54d68b39c0744a
Matthew Smart <matthewsmart13@gmail.com>
2025-02-11 01:43:49 -0600

    util for getting last bin

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,6 +1,14 @@
 package utils
 
-import "strings"
+import (
+	"context"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/itsMe-ThatOneGuy/parts-bin/internal/database"
+	"github.com/itsMe-ThatOneGuy/parts-bin/internal/models"
+	"github.com/itsMe-ThatOneGuy/parts-bin/internal/state"
+)
 
 func ParseInputPath(s string) (last string, parent string, pathSlice []string) {
 	splitSlice := strings.Split(s, "/")
@@ -24,3 +32,34 @@ func ValidateFlags(flags map[string]struct{}, key string) bool {
 	_, exists := flags[key]
 	return exists
 }
+
+func GetLastBin(s *state.State, path string) (models.Bin, error) {
+	_, _, pathSlice := ParseInputPath(path)
+
+	parentID := uuid.NullUUID{Valid: false}
+	for i, e := range pathSlice {
+		bin, err := s.DBQueries.GetBin(context.TODO(), database.GetBinParams{
+			Name:      e,
+			ParentBin: parentID,
+		})
+		if err != nil {
+			return models.Bin{}, err
+		}
+
+		if i == len(pathSlice)-1 {
+			last := models.Bin{
+				Name:     e,
+				ID:       uuid.NullUUID{Valid: true, UUID: bin.ID},
+				ParentID: parentID,
+			}
+
+			return last, nil
+
+		}
+
+		parentID = uuid.NullUUID{Valid: true, UUID: bin.ID}
+	}
+
+	return models.Bin{}, nil
+}
+
