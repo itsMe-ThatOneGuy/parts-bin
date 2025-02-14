df70ed5eec0fbe54f2d1b4a197f1d74d1c28ac41
Matthew Smart <matthewsmart13@gmail.com>
2025-02-14 14:33:17 -0600

    schema verstion 2 adding parts table

---
+++ sql/schema/002_parts.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE parts (
+    part_id SERIAL PRIMARY KEY,
+    id UUID UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL,
+    sku TEXT UNIQUE NOT NULL,
+    parent_id UUID NOT NULL 
+    REFERENCES bins(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE parts;
