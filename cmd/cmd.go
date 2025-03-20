197b9bd67b23f80a03dd7b3c70e8e14f0ea6dfbf
Matthew Smart <matthewsmart13@gmail.com>
2025-03-19 23:40:30 -0500

    added commands to command slice

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -10,4 +10,24 @@ var commands = []models.Command{
 		Description: "Create a bin in provided path",
 		Callback:    CreateBin,
 	},
+	{
+		Name:        "part",
+		Description: "Create a part in provided path",
+		Callback:    CreatePart,
+	},
+	{
+		Name:        "ls",
+		Description: "List parts and bins in provided path",
+		Callback:    Ls,
+	},
+	{
+		Name:        "mv",
+		Description: "move a part/bin from provided source path to provided destination path",
+		Callback:    Mv,
+	},
+	{
+		Name:        "rm",
+		Description: "remove part/bin in provided path",
+		Callback:    Rm,
+	},
 }
