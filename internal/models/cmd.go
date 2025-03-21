e59a8e58121d59227e2aaaf98ae4b7dd66c5c5ad
Matthew Smart <matthewsmart13@gmail.com>
2025-03-21 00:11:11 -0500

    removed unused file

--- internal/models/cmd.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/itsMe-ThatOneGuy/parts-bin/internal/state"
-
-type Command struct {
-	Name        string
-	Description string
-	Callback    func(*state.State, map[string]struct{}, []string) error
-}
