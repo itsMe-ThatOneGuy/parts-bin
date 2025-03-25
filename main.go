d7ba2edd630912b5d1758e117789ecdc75c26dc1
Matthew Smart <matthewsmart13@gmail.com>
2025-03-24 20:16:36 -0500

    updated main with repl and runner calls

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"strings"
 
-	"github.com/itsMe-ThatOneGuy/parts-bin/cmd/bins"
+	"github.com/itsMe-ThatOneGuy/parts-bin/cmd"
 	"github.com/itsMe-ThatOneGuy/parts-bin/internal/state"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: cli <command> [args...]")
-		return
-	}
-
-	command := os.Args[1]
-	input := os.Args[2:]
-
-	hasFlags := false
-	flags := make(map[string]struct{})
-	if strings.HasPrefix(input[0], "-") {
-		hasFlags = true
-		_flags := strings.Split(input[0], "")[1:]
-		for _, e := range _flags {
-			flags[string(e)] = struct{}{}
-		}
-	}
-
-	args := input
-	if hasFlags {
-		args = input[1:]
-	}
-
 	state := &state.State{}
 	err := state.InitConfig()
 	if err != nil {
@@ -51,25 +25,9 @@ func main() {
 	}
 	defer state.CloseDB()
 
-	switch command {
-	case "createBin":
-		bin, err := bins.CreateBin(state, args)
-		if err != nil {
-			log.Fatalf("Error creating bin: %v", err)
-		}
-		fmt.Println(bin.Name)
-	case "getBin":
-		bin, err := bins.GetBin(state, args)
-		if err != nil {
-			log.Fatalf("Error creating bin: %v", err)
-		}
-		fmt.Println(bin)
-	case "deleteBin":
-		bin, err := bins.GetBin(state, args)
-		if err != nil {
-			log.Fatalf("Error deleting bin: %v", err)
-		}
-		fmt.Println(bin)
+	if len(os.Args) == 1 {
+		cmd.Repl(state)
+		return
 	}
 
 	cmd.RunCommand(state)
