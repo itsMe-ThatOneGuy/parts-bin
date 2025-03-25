faac79c790de5b05c27e545f57d8c25d7ee89541
Matthew Smart <matthewsmart13@gmail.com>
2025-03-24 20:14:50 -0500

    repl logic for app repl mode

---
+++ cmd/repl.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/itsMe-ThatOneGuy/parts-bin/internal/state"
+	"github.com/itsMe-ThatOneGuy/parts-bin/internal/utils"
+)
+
+func Repl(s *state.State) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("parts-bin > ")
+
+		scanner.Scan()
+		input := parseInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		cmdName := input[0]
+		if cmdName == "exit" {
+			break
+		}
+
+		hasFlags := false
+		flags := make(map[string]struct{})
+		if len(input) > 1 {
+			if strings.HasPrefix(input[1], "-") {
+				flags = utils.ParseFlags(input, &hasFlags)
+			}
+		}
+
+		args := input[1:]
+		if hasFlags {
+			args = input[2:]
+		}
+
+		command, ok := Commands()[cmdName]
+		if ok {
+			err := command.Callback(s, flags, args)
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown Command")
+			continue
+		}
+
+	}
+}
+
+func parseInput(input string) []string {
+	return strings.Fields(input)
+}
