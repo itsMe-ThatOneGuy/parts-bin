76898d815aaf678773a97f32900a72f1a6863a1e
Matthew Smart <matthewsmart13@gmail.com>
2025-03-24 20:14:37 -0500

    command runner for single commands

---
+++ cmd/runner.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/itsMe-ThatOneGuy/parts-bin/internal/state"
+	"github.com/itsMe-ThatOneGuy/parts-bin/internal/utils"
+)
+
+func RunCommand(s *state.State) {
+	input := os.Args[1:]
+	cmdName := input[0]
+
+	hasFlags := false
+	flags := make(map[string]struct{})
+	if len(input) > 1 {
+		flags = utils.ParseFlags(input, &hasFlags)
+	}
+
+	args := input[1:]
+	if hasFlags {
+		args = input[2:]
+	}
+
+	command, ok := Commands()[cmdName]
+	if ok {
+		err := command.Callback(s, flags, args)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println("Unknown Command")
+	}
+}
