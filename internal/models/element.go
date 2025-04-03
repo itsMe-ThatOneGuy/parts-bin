6dcd8042b6951133e74d3388a32b276e2f9a3d5a
Matthew Smart <matthewsmart13@gmail.com>
2025-04-02 22:51:13 -0500

    added ParentName and Path to model

--- internal/models/element.go
+++ internal/models/element.go
@@ -3,9 +3,11 @@ package models
 import "github.com/google/uuid"
 
 type Element struct {
-	Type     string
-	Name     string
-	Sku      string
-	ID       uuid.NullUUID
-	ParentID uuid.NullUUID
+	Type       string
+	Name       string
+	Sku        string
+	ID         uuid.NullUUID
+	ParentName string
+	ParentID   uuid.NullUUID
+	Path       string
 }
